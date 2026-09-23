ae4e3be3f014744f0571e4da36dc1edc846f1b1f
danennh11 <danendranayott@gmail.com>
2026-09-23 17:59:01 +0700

    pertemuan-04: Level 6 selesai

--- pertemuan-04/src/components/PenghitungBatas.tsx
+++ pertemuan-04/src/components/PenghitungBatas.tsx
@@ -3,6 +3,18 @@
 // "+" dan "-". Tombol "+" harus disabled saat angka sudah = max, tombol "-"
 // harus disabled saat angka sudah = min.
 // Lihat SOAL.md untuk kontrak lengkap.
-export function PenghitungBatas(props: any) {
-  return <p>TODO</p>
+//
+import { useState } from "react";
+
+type hitungBatasProps = {
+  min: number;
+  max: number;
+}
+export function PenghitungBatas({min,max}: hitungBatasProps) {
+  const [state, setState] = useState(min)
+  return <div>
+    <button disabled={state === max} onClick={() => setState(state + 1)}>+</button>
+    <button disabled={state === min} onClick={() => setState(state - 1)}>-</button>
+    <p>Nilai: {state}</p>
+  </div>
 }
